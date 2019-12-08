c18ac5c9c61c8cbd9edff507f9dc5d5acb25ab75
libbyschuknight <libbysk@gmail.com>
2019-12-08 15:30:50 +1300

    Complete more lessons
    3 Conditional Rendering
    4 List Rendering

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 var app = new Vue({
   // Vue Instance
-  el: '#app', // element e.g. id="app"
+  el: '#app', // element e.g. id='app'
   data: {
     product: 'Socks', // Vue is reactive = change product here, will change in view, anywhere
     description: 'This is a description for the product',
     image: 'vmSocks-green-onWhite.jpg',
+    inventory: 8,
+    inStock: true,
+    onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ],
+    sizes: ['extra-small', 'small', 'medium', 'large'],
     altText: 'A pair of socks',
     url:
       'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
