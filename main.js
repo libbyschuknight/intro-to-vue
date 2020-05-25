a841c6ea950965a4733e8474ab7056952c136f63
libbyschuknight <libbysk@gmail.com>
2020-05-25 12:03:23 +1200

    7. computed properties

--- main.js
+++ main.js
@@ -2,23 +2,27 @@ var app = new Vue({
   // Vue Instance
   el: '#app', // element e.g. id='app'
   data: {
+    brand: 'Schumacher-Knight',
     product: 'Socks', // Vue is reactive = change product here, will change in view, anywhere
     description: 'This is a description for the product',
-    image: 'vmSocks-green-onWhite.jpg',
-    inventory: 8,
-    inStock: true,
+    selectedVariant: 0,
+    // image: 'vmSocks-green-onWhite.jpg',
+    // inventory: 8,
+    // inStock: true,
     onSale: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: 'vmSocks-green-onWhite.jpg'
+        variantImage: 'vmSocks-green-onWhite.jpg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: 'vmSocks-blue-onWhite.jpg'
+        variantImage: 'vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0
       }
     ],
     sizes: ['extra-small', 'small', 'medium', 'large'],
@@ -35,8 +39,26 @@ var app = new Vue({
     removeFromCart() {
       this.cart -= 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage
+    },
+    inStock() {
+      this.variants[this.selectedVariant].variantQuantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      }
+      return this.brand + ' ' + this.product + ' are not on sale'
     }
   }
 });
